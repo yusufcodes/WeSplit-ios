8ec2319cea981190354e37e994e122385d57f586
Yusuf <yusufcodes@outlook.com>
2022-06-14 15:36:47 +0100

    feat: add logic and ui for tip calculator

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Specify state values using @State
+    @State private var checkAmount = 0.0
+    @State private var numPeople = 0
+    @State private var totalTip = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [0, 5, 10, 20]
+    
+    // Computed Value
+    var totalToPay: Double {
+        let totalToPay = checkAmount + (checkAmount * (Double(totalTip) / 100) )
+        let totalPerPerson = totalToPay / Double(numPeople + 2)
+        return totalPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter your check amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "GBP"))
+                        .keyboardType(UIKeyboardType.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $totalTip) {
+                        // Iterate over tipPercentages and each number in the array is the unique identifier
+                        ForEach(tipPercentages, id:\.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much would you like to tip?")
+                }
+                
+                Section {
+                    Text(totalToPay, format: .currency(code: Locale.current.currencyCode ?? "GBP"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
